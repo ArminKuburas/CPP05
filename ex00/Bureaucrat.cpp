dde2c185427f7d23697312d8079cbc6f4d701437
ArminKuburas <akuburass@gmail.com>
2024-09-17 04:01:19 +0300

    feat: added everything needed from exercise 00

---
+++ ex00/Bureaucrat.cpp
@@ -0,0 +1,84 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.cpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akuburas <akuburas@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/17 03:47:55 by akuburas          #+#    #+#             */
+/*   Updated: 2024/09/17 03:53:53 by akuburas         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Bureaucrat.hpp"
+
+Bureaucrat::Bureaucrat() : _name("default"), _grade(150)
+{
+}
+
+Bureaucrat::Bureaucrat(const std::string name, int grade) : _name(name)
+{
+	if (grade < 1)
+		throw Bureaucrat::GradeTooHighException();
+	else if (grade > 150)
+		throw Bureaucrat::GradeTooLowException();
+	_grade = grade;
+	std::cout << "Bureaucrat " << _name << " created." << " With a grade of " << _grade << std::endl;
+}
+
+Bureaucrat::Bureaucrat(const Bureaucrat &source) : _name(source._name), _grade(source._grade)
+{
+}
+
+Bureaucrat::~Bureaucrat()
+{
+	std::cout << "Bureaucrat " << _name << " destroyed." << std::endl;
+}
+
+Bureaucrat &Bureaucrat::operator=(const Bureaucrat &source)
+{
+	if (this == &source)
+		return (*this);
+	_grade = source._grade;
+	return (*this);
+}
+
+const std::string Bureaucrat::getName() const
+{
+	return (_name);
+}
+
+int Bureaucrat::getGrade() const
+{
+	return (_grade);
+}
+
+void Bureaucrat::incrementGrade()
+{
+	if (_grade - 1 < 1)
+		throw Bureaucrat::GradeTooHighException();
+	_grade--;
+}
+
+void Bureaucrat::decrementGrade()
+{
+	if (_grade + 1 > 150)
+		throw Bureaucrat::GradeTooLowException();
+	_grade++;
+}
+
+const char *Bureaucrat::GradeTooHighException::what() const throw()
+{
+	return ("Grade is too high.");
+}
+
+const char *Bureaucrat::GradeTooLowException::what() const throw()
+{
+	return ("Grade is too low.");
+}
+
+std::ostream &operator<<(std::ostream &out, const Bureaucrat &bureaucrat)
+{
+	out << bureaucrat.getName() << ", bureaucrat grade " << bureaucrat.getGrade() << ".";
+	return (out);
+}
