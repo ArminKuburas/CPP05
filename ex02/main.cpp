4018e3fa8cd0e3a002af3aa29580c9e99c084375
ArminKuburas <akuburass@gmail.com>
2024-10-01 11:46:46 +0300

    feat: added Makefile and main to ex02

---
+++ ex02/main.cpp
@@ -0,0 +1,37 @@
+#include "Bureaucrat.hpp"
+#include "ShrubberyCreationForm.hpp"
+#include "RobotomyRequestForm.hpp"
+#include "PresidentialPardonForm.hpp"
+
+int main(void)
+{
+	try
+	{
+		Bureaucrat bob("Bob", 45);
+		Bureaucrat alice("Alice", 5);
+		
+		ShrubberyCreationForm shrubForm("home");
+		RobotomyRequestForm robotForm("target1");
+		PresidentialPardonForm pardonForm("target2");
+
+		bob.signForm(shrubForm);
+		bob.executeForm(shrubForm);
+
+		std::cout << "------------------" << std::endl;
+
+		bob.signForm(robotForm);
+		bob.executeForm(robotForm);
+
+		std::cout << "------------------" << std::endl;
+
+		alice.signForm(pardonForm);
+		alice.executeForm(pardonForm);
+
+	}
+	catch (const std::exception &e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+
+	return 0;
+}
